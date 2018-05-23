09e890b391941c11314a6d52bc31c92253e88bb2
maxzo <maximilianosariego@gmail.com>
2018-05-23 11:07:38 -0300

    Moved toast to top.

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -65,7 +65,7 @@ export class HomePage {
     let toast = this.toastCtrl.create({
       message: 'Copiado',
       duration: 1000,
-      position: 'bottom'
+      position: 'top'
     });
 
     toast.present();
